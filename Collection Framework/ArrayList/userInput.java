d07c2a879ded1e67d2457cc443f186390c31dc0b
Sahil Kumar <sahilkumar8617@gmail.com>
2025-06-13 11:37:50 +0530

    modified the userInput file

--- Collection Framework/ArrayList/userInput.java
+++ Collection Framework/ArrayList/userInput.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 public class userInput {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        ArrayList<Integer> myList=new ArrayList<>();
-        System.out.println("how many digit you want to store in the ArrayList :");
-        int number=sc.nextInt();
-
-        for(int i=0;i<number;i++){
-            System.out.println("enter the element:");
-            int num=sc.nextInt();
-            myList.add(num);
-
-        }
-        System.out.println(" the numbers in the list are:");
-        for(int j:myList){
-
-            
-            System.out.println(j);
-        }
         
+        System.out.println("enter the size of arraylist:");
+        int size=sc.nextInt();
+
+        ArrayList<Integer> mylist=new ArrayList<Integer>(size);
+
+        for(int i=0;i<size;i++){
+            System.out.println("enter the element into mylist:");
+            int element=sc.nextInt();
+            mylist.add(element);
+        }
+        System.out.println(mylist);
+
+
     }
     
 }
